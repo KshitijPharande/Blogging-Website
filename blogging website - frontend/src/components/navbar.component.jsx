e8309cd187d384d78775ed0611fd56034ab40f09
Reaper1737 <pharandekshitij@gmail.com>
2024-07-18 14:24:42 +0530

    Working Search Input Configured

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import logo from "../imgs/logo.png";
 import { UserContext } from '../App';
 import UserNavigationPanel from "./user-navigation.component";
@@ -7,6 +7,8 @@ import UserNavigationPanel from "./user-navigation.component";
 const Navbar = () => {
     const [searchBoxVisibility, setSearchBoxVisibility] = useState(false)
     const [ userNavPanel, setUserNavPanel ] = useState(false);
+    
+    let navigate = useNavigate();
 
     const { userAuth: { access_token, profile_img } = {} } = useContext(UserContext);
 
@@ -14,11 +16,22 @@ const Navbar = () => {
         setUserNavPanel(currentVal => !currentVal);
     }
 
+
+    const handleSearch = (e) => {
+        let query = e.target.value;
+        console.log(e);
+        if (e.key === 'Enter' && query.length) {
+            navigate(`/search/${query}`);
+        }
+    }
+    
+
     const handleBlur = () =>{
         setTimeout(() =>{
             setUserNavPanel(false);
         }, 200);
     }
+
 
     return (
       <>
@@ -31,7 +44,8 @@ const Navbar = () => {
                     type="text"
                     placeholder="Search"
                     className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
-                />
+                    onKeyDown={handleSearch}    
+                        />
                 <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
             </div>
             <div className="flex items-center gap-3 md:gap-6 ml-auto">
