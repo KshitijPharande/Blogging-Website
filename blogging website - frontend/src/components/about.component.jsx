75eaa65e8e37c7860fde68195405b5f74e92bbcc
Reaper1737 <pharandekshitij@gmail.com>
2024-07-22 18:14:54 +0530

    User/About-User Info Page Shown onf User Profile

--- blogging website - frontend/src/components/about.component.jsx
+++ blogging website - frontend/src/components/about.component.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+import { getFullDay } from "../common/date";
 const AboutUser =({ className,bio, social_links, joinedAt}) =>{
     return(
        <div className={"md:w-[90%] md:mt-7 " + className}>
@@ -5,10 +7,22 @@ const AboutUser =({ className,bio, social_links, joinedAt}) =>{
 
         <div className="flex gap-x-7 gap-y-2 flex-wrap my-7 items-center
         text-dark-grey">
-            
+
+            {
+                Object.keys(social_links).map((key)=>{
+
+                    let link = social_links[key];
+
+                    return link ? <Link to={link} key={key} target="_blank" > <i className={"fi " + (key != 'website' ? "fi-brands-" + key : "fi-rr-globe")
+                        + " text -2xl hover: text-black"
+                    }>
+                    </i></Link>
+                    : ""
+                })
+            }
 
         </div>
-
+            <p className="text-dark-grey text-xl leading-7">Joined On {getFullDay(joinedAt)}</p>
        </div>
     )
 }
