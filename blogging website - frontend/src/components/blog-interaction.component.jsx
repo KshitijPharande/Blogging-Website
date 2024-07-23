b8c1598f1d4448cc7cc69965ffb8a1d95d43689b
Reaper1737 <pharandekshitij@gmail.com>
2024-07-23 18:37:54 +0530

    Enhance BlogPage component with loader, animation, and blog interaction features

---
+++ blogging website - frontend/src/components/blog-interaction.component.jsx
@@ -0,0 +1,8 @@
+const BlogIntereaction = () =>{
+    return(
+
+        <h1> this is blog intereaction</h1>
+    )
+}
+
+export default BlogIntereaction
--- blogging website - frontend/src/pages/blog.page.jsx
+++ blogging website - frontend/src/pages/blog.page.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import AnimationWrapper from "../common/page-animation";
+import Loader from "../components/loader.component";
+import { Link } from "react-router-dom";
+import { getDay, getFullDay } from "../common/date";
+import BlogIntereaction from "../components/blog-interaction.component";
 
 export const blogStructure = {
     title: '',
     des: '',
     content: [],
     tags: [],
-    author: {personal_info: {  } },
+    author: { personal_info: {} },
     banner: '',
     publishedAt: '',
 }
@@ -16,17 +21,19 @@ const BlogPage = () => {
     let { blog_id } = useParams();
 
     const [blog, setBlog] = useState(blogStructure);
+    const [loading, setLoading] = useState(true);
 
-    let {title, content, banner,
-     author: {personal_info: {fullname, username, profile_img}}, publishedAt} = blog;
+    const { title, content, banner, author: { personal_info: { fullname, username: author_username, profile_img } }, publishedAt } = blog;
 
     const fetchBlog = () => {
         axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", { blog_id })
             .then(({ data: { blog } }) => {
                 setBlog(blog);
+                setLoading(false);
             })
             .catch(err => {
                 console.log(err);
+                setLoading(false);
             });
     };
 
@@ -35,13 +42,32 @@ const BlogPage = () => {
     }, []);
 
     return (
-        <div>
-            {blog ? (
-                <h1>This is Blogid {blog.title}</h1>
-            ) : (
-                <p>Loading...</p>
+        <AnimationWrapper>
+            {loading ? <Loader /> : (
+                <div className="max-w-[900px] center py-10 max-lg:px-5[5vw]">
+                    <img src={banner} className="aspect-video" alt="Blog Banner" />
+                    <div className="mt-12">
+                        <h1 className="text-3xl font-bold">{title}</h1>
+                        <div className="flex max-sm:flex-col justify-between my-8">
+                            <div className="flex gap-5 items-start">
+                                <img src={profile_img} className="w-12 h-12 rounded-full" alt="Author" />
+                                <p className="capitalize">
+                                    {fullname}
+                                    <br />
+                                    @
+                                    <Link to={`/user/${author_username}`} className="underline">{author_username}</Link>
+                                </p>
+                            </div>
+                            <p className="text-dark-grey opacity-75 max-sm:mt-6
+                            max-sm:ml-12 max-sm: pl-5">
+                                Published on {getDay(publishedAt)}
+                            </p>
+                        </div>
+                    </div>
+                    <BlogIntereaction/>
+                </div>
             )}
-        </div>
+        </AnimationWrapper>
     );
 };
 
