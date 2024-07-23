f515258801d38cc197ece77555dbde89d576deb7
Reaper1737 <pharandekshitij@gmail.com>
2024-07-23 17:39:54 +0530

    Add BlogPage component with blog fetching and rendering functionality

--- blogging website - frontend/src/pages/blog.page.jsx
+++ blogging website - frontend/src/pages/blog.page.jsx
@@ -1,15 +1,48 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-const BlogPage = () =>{
+import axios from "axios";
 
-    let {blog_id } = useParams()
-
-    return(
-        <h1> This is Blogid {blog_id} </h1>
-        
-
-    )
-
-
+export const blogStructure = {
+    title: '',
+    des: '',
+    content: [],
+    tags: [],
+    author: {personal_info: {  } },
+    banner: '',
+    publishedAt: '',
 }
+
+const BlogPage = () => {
+    let { blog_id } = useParams();
+
+    const [blog, setBlog] = useState(blogStructure);
+
+    let {title, content, banner,
+     author: {personal_info: {fullname, username, profile_img}}, publishedAt} = blog;
+
+    const fetchBlog = () => {
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-blog", { blog_id })
+            .then(({ data: { blog } }) => {
+                setBlog(blog);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
+    useEffect(() => {
+        fetchBlog();
+    }, []);
+
+    return (
+        <div>
+            {blog ? (
+                <h1>This is Blogid {blog.title}</h1>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
+};
 
 export default BlogPage;
