a042ce9ed865244c2d4ae1ad802d8ff42070518f
Reaper1737 <pharandekshitij@gmail.com>
2024-07-30 21:41:26 +0530

    Add reply functionality to comments and threaded comment display

--- blogging website - frontend/src/components/comment-field.component.jsx
+++ blogging website - frontend/src/components/comment-field.component.jsx
@@ -4,7 +4,7 @@ import toast, { Toaster } from "react-hot-toast";
 import axios from "axios";
 import { BlogContext } from "../pages/blog.page";
 
-const CommentField = ({ action }) =>{
+const CommentField = ({ action, index= undefined, replyingTo= undefined, setReplying }) =>{
 
     let {blog, blog: {_id, author: {_id: blog_author},comments, comments:{results: commentsArr}, activity, activity: {total_comments, total_parent_comments}}, setBlog, setTotalParentCommentsLoaded  } = useContext(BlogContext)
 
@@ -21,7 +21,7 @@ const CommentField = ({ action }) =>{
         }
 
         axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/add-comment",{
-            _id, blog_author, comment
+            _id, blog_author, comment, replying_to: replyingTo
         },{
             headers: {
                 "Authorization": `Bearer ${access_token}`
@@ -34,11 +34,29 @@ const CommentField = ({ action }) =>{
 
             let newCommentArr;
 
-            data.childrenLevel = 0;
+            if(replyingTo){
 
-            newCommentArr = [ data, ...commentsArr];
+                commentsArr[index].children.push(data._id)
+                data.childrenLevel = commentsArr[index].childrenLevel + 1;
+                data.parentIndex = index;
 
-            let parentCommentIncrementVal = 1;
+                commentsArr[index].isReplyLoaded = true
+
+                commentsArr.splice(index + 1, 0, data);
+
+                newCommentArr = commentsArr
+
+                setReplying(false);
+
+            }else{
+                data.childrenLevel = 0;
+
+                newCommentArr = [ data, ...commentsArr];
+            }
+
+          
+
+            let parentCommentIncrementVal = replyingTo ? 0 : 1;
 
             setBlog({...blog, comments: {...comment, results: newCommentArr}, activity: { ...activity, total_comments: total_comments + 1, total_parent_comments: total_parent_comments + parentCommentIncrementVal } })
 
--- server/server.js
+++ server/server.js
@@ -497,36 +497,68 @@ server.post("/isliked-by-user", verifyJWT, (req, res) => {
 
 server.post("/add-comment", verifyJWT, (req, res) => {
     let user_id = req.user;
-    let { _id, comment, blog_author } = req.body;
+    let { _id, comment, blog_author, replying_to } = req.body;
 
     if (!comment.length) {
         return res.status(403).json({ error: "Write something to leave a comment" });
     }
-    // Creating Comment
-    let commentObj = new Comment( {
-        blog_id: _id, blog_author,comment, commented_by: user_id,
-        
-    })
-    commentObj.save().then(commentFile =>{
-        let { comment, commentedAt, children  } = commentFile;
-        Blog.findOneAndUpdate({_id}, { $push: {"comments": commentFile._id },
-            $inc: { "activity.total_comments": 1, "activity.total_parent_comments": 1 }, })
-            .then(blog =>{
-                console.log("New comment created")
-            })
-
-            let notificationObj ={
-                type: "comment",
-                blog: _id,
-                notification_for: blog_author,
-                user: user_id,
-                comment: commentFile._id
-            }
-            new Notification(notificationObj).save().then(notification => console.log("new noti created"))
-            return res.status(200).json({ comment, commentedAt, _id: commentFile._id, user_id, children });
-    })
     
+    // Creating Comment
+    let commentObj = {
+        blog_id: _id, 
+        blog_author, 
+        comment, 
+        commented_by: user_id
+    };
+    if (replying_to) {
+        commentObj.parent = replying_to;
+    }
+
+    new Comment(commentObj).save().then(async commentFile => {
+        let { comment, commentedAt, children } = commentFile;
+        Blog.findOneAndUpdate(
+            { _id }, 
+            { 
+                $push: { "comments": commentFile._id },
+                $inc: { 
+                    "activity.total_comments": 1, 
+                    "activity.total_parent_comments": replying_to ? 0 : 1 
+                } 
+            }
+        ).then(blog => {
+            console.log("New comment created");
+        });
+
+        let notificationObj = {
+            type: replying_to ? "reply" : "comment",
+            blog: _id,
+            notification_for: blog_author,
+            user: user_id,
+            comment: commentFile._id
+        };
+
+        if (replying_to) {
+            notificationObj.replied_on_comment = replying_to;
+
+            await Comment.findOneAndUpdate(
+                { _id: replying_to }, 
+                { $push: { children: commentFile._id } }
+            ).then(replyingToCommentDoc => {
+                notificationObj.notification_for = replyingToCommentDoc.commented_by;
+            });
+        }
+
+        new Notification(notificationObj).save().then(notification => {
+            console.log("New notification created");
+        });
+
+        return res.status(200).json({ comment, commentedAt, _id: commentFile._id, user_id, children });
+    }).catch(err => {
+        console.error(err);
+        return res.status(500).json({ error: "An error occurred while saving the comment." });
+    });
 });
+
 
 server.post("/get-blog-comments",(req, res ) =>{
     let { blog_id, skip } = req.body;
