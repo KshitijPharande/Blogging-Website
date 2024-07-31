a39c41ec8230029ba3d9b4f54469a3e157157134
Reaper1737 <pharandekshitij@gmail.com>
2024-08-01 03:28:01 +0530

    Add '/update-profile-img' route to update profile image URL in database

--- server/server.js
+++ server/server.js
@@ -387,6 +387,28 @@ User.findOne({ "personal_info.username": username })
 
 })
 
+server.post('/update-profile-img', verifyJWT, (req, res)=>{
+
+
+    let { url } = req.body;
+
+    User.findOneAndUpdate({ _id: req.user }, { "personal_info.profile_img": url })
+    .then(() =>{
+        return res.status(200).json({ profile_img: url })
+    })
+    .catch(err =>{
+        return res.status(500).json({ error: err.message })
+    })
+})
+
+
+
+
+
+
+
+
+
 
 server.post('/create-blog', verifyJWT, (req, res) => {
     let authorID = req.user;
